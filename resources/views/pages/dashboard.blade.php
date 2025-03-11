fc1d48a895bb0e7b203b22d229c418623fbe805d
Kate <cathrinakatebolanos@gmail.com>
2025-03-12 06:18:20 +0800

    updated dashboard style

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -44,17 +44,17 @@
             </div>
         </div>
         <!-- Main row -->
-        <div class="row">
+        <div class="row dashboard-card">
 
             <div class="col-12 col-lg-4">
                 <div class="card">
                     <div class="card-header ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title">
-                            <i class="fas fa-chart-pie mr-1"></i>
+                            <i class="fas fa-lightbulb mr-1"></i>
                             Total Energy Consumption
                         </h3>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body energy-bg">
                         <div id="totalEnergyConsumption" class="card-box d-flex flex-column justify-content-center align-items-center ">
                             <h1 id="totalEnergyConsumptionValue" class="dashboard-value">0 </h1>
                             <h6>kWh</h6>
@@ -67,7 +67,7 @@
                 <div class="card">
                     <div class="card-header ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title">
-                            <i class="fas fa-chart-pie mr-1"></i>
+                            <i class="fas fa-plug mr-1"></i>
                             Previous & Present Energy Consumption - All Meters
                         </h3>
                     </div>
@@ -81,11 +81,11 @@
                 <div class="card">
                     <div class="card-header ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title">
-                            <i class="fas fa-chart-pie mr-1"></i>
+                            <i class="fas fa-calendar-alt mr-1"></i>
                             Current Month Energy Consumption
                         </h3>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body energy-bg">
                         <div id="currentMonthEnergyConsumption" class="card-box">
                             <div class="currentMonthEnergy">
                                 <h1 id="currentMonthEnergyConsumptionValue" class="dashboard-value">0 </h1>
@@ -104,12 +104,12 @@
 
         </div>
 
-        <div class="row">
+        <div class="row dashboard-card">
             <div class="col-12 col-lg-4">
                 <div class="card">
                     <div class="card-header ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title">
-                            <i class="fas fa-chart-pie mr-1"></i>
+                            <i class="fa fa-chart-bar mr-1"></i>
                             Daily Energy Consumption Per Meter
                         </h3>
                     </div>
@@ -125,7 +125,7 @@
                 <div class="card">
                     <div class="card-header ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title">
-                            <i class="fas fa-chart-pie mr-1"></i>
+                            <i class="fas fa-search-location mr-1"></i>
                             Energy Consumption Per Area
                         </h3>
                     </div>
@@ -155,33 +155,33 @@
                 <div class="card">
                     <div class="card-header ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title">
-                            <i class="fas fa-chart-pie mr-1"></i>
+                            <i class="fas fa-broadcast-tower mr-1"></i>
                             Carbon Footprint
                         </h3>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body ">
                         <section class="col-12 connectedSortable">
-                            <div class="card-box">
-                                <div class="col-md-12">
-                                    <h5>GHG Emission (kg of CO2) - Current Day</h5>
-                                    <div class="progress " role="progressbar"
-                                        aria-label="Example with label" aria-valuenow="100" aria-valuemin="0"
-                                        aria-valuemax="100" style="height: 50px;">
-                                        <div class="progress-bar progress-bar-striped progress-bar-animated" id="ghgCurrentDay" style="width: 0%">0 kWh</div>
+                            <div class="card-box ghg">
+                                <div>
+                                    <div class="col-md-12 ghgday">
+                                        <h5>GHG Emission (kg of CO2) - Current Day</h5>
+                                        <h4 id="ghgCurrentDayValue">0 kWh</h4>
+                                        <div class="progress " role="progressbar"
+                                            aria-label="Example with label" aria-valuenow="100" aria-valuemin="0"
+                                            aria-valuemax="100" style="height: 50px;">
+                                            <div class="progress-bar progress-bar-striped progress-bar-animated" id="ghgCurrentDay" style="width: 0%"></div>
+                                        </div>
                                     </div>
-                                    <h5 id="ghgCurrentDayValue">0 kWh</h5>
-                                </div>
-                                <br>
-                                <br>
-                                <br>
-                                <div class="col-md-12">
-                                    <h5>GHG Emission (kg of CO2) - Current Month</h5>
-                                    <div class="progress " role="progressbar"
-                                        aria-label="Example with label" aria-valuenow="25" aria-valuemin="0"
-                                        aria-valuemax="100" style="height: 50px;">
-                                        <div class="progress-bar progress-bar-striped progress-bar-animated" id="ghgCurrentMonth" style="width: 0%">0 kWh</div>
+                                    <div class="col-md-12 ghgmonth">
+                                        <h5>GHG Emission (kg of CO2) - Current Month</h5>
+                                        <h4 id="ghgCurrentMonthValue">0 kWh</h4>
+                                        <div class="progress " role="progressbar"
+                                            aria-label="Example with label" aria-valuenow="25" aria-valuemin="0"
+                                            aria-valuemax="100" style="height: 50px;">
+                                            <div class="progress-bar progress-bar-striped progress-bar-animated" id="ghgCurrentMonth" style="width: 0%"></div>
+                                        </div>
+                                        
                                     </div>
-                                    <h5 id="ghgCurrentMonthValue">0 kWh</h5>
                                 </div>
                             </div>
                         </section>
