3f3bd7bbae30281efc84bdfa96045e52fca098f5
Kate <cathrinakatebolanos@gmail.com>
2025-04-01 03:58:23 +0800

    main js v1.3

--- public/assets/js/energyConsumption.js
+++ public/assets/js/energyConsumption.js
@@ -1,4 +1,4 @@
-import { fetchData, setIntervalAtFiveMinuteMarks, charts, formatDate, renderChart, getStartEndDate, colorScheme, exportFn, createOdometer } from "./shared/main.js?v=1.2";
+import { fetchData, setIntervalAtFiveMinuteMarks, charts, formatDate, renderChart, getStartEndDate, colorScheme, exportFn, createOdometer } from "./shared/main.js?v=1.3";
 
 colorScheme();
 
