380e5dd87f63b5428d6c7400e1028965dc19f979
Kate <cathrinakatebolanos@gmail.com>
2025-04-01 03:01:45 +0800

    css version

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -32,5 +32,5 @@
 <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
 <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
 <!-- CSS -->
-<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}?v={{ time() }}">
+<link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}?v={{ time() }}">
