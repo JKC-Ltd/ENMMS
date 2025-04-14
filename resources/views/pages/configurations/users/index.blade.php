74431653750a3b4b4991650581c6a50999b6c018
nicolecarillo <healer.moebus@gmail.com>
2025-04-15 01:09:39 +0800

    Users Datatables

--- resources/views/pages/configurations/users/index.blade.php
+++ resources/views/pages/configurations/users/index.blade.php
@@ -64,7 +64,7 @@
             </div>
         </div>
     </x-slot>
-    <x-slot name="importedScripts">
+    @section('scripts')
         @include('includes.datatables-scripts')
         <script src="{{ asset('assets/js/datatables.js') }}"></script>
         <script src="{{ asset('assets/js/sweetalert2.all.min.js') }}"></script>
@@ -86,5 +86,5 @@
                 @endif
             });
         </script>
-    </x-slot>
+    @endsection
 </x-app-layout>
